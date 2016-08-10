eabf1abe7a31d30aefbc215ef8122edec22d47af
Daniel Laikhter <daniellaikhter@gmail.com>
2016-08-10 16:20:09 -0400

    removed original checkers with generated maven project, more UI changes to come

--- checkers-project/src/main/java/com/cs451/checkers/ChessBoard.java
+++ checkers-project/src/main/java/com/cs451/checkers/ChessBoard.java
@@ -1,4 +1,4 @@
-package application;
+package com.cs451.checkers;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
--- checkers-project/src/main/java/com/cs451/checkers/Main.java
+++ checkers-project/src/main/java/com/cs451/checkers/Main.java
@@ -1,7 +1,21 @@
 package com.cs451.checkers;
 
-class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("Checkers");
+	        primaryStage.setScene(new Menu().getScene(primaryStage));
+	        primaryStage.show();
+	    } catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args){
+		launch(args);
+	}
 }
