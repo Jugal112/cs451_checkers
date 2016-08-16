884c349f745bdbdf1d9f8103c6950021bc2a3a5e
ajc-developer <ajc.developer@gmail.com>
2016-08-16 18:23:43 -0400

    cross platform for loading resources

--- checkers-project/src/main/java/com/cs451/checkers/Browser.java
+++ checkers-project/src/main/java/com/cs451/checkers/Browser.java
@@ -25,7 +25,8 @@ class Browser extends Region {
         window.setMember("javaOp", new JavaOps());
 
         //load the web page
-        webEngine.load("file:/" + path + "/src/resources/index.html");
+        //webEngine.load("file:/" + path + "/src/resources/index.html");
+        webEngine.load(getClass().getResource("/index.html").toExternalForm());
     }
 
     public Object executeJavascript(String js) {
