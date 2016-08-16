e6e7194b4433b418e353acf2c148b5bc4ad7f538
Daniel Laikhter <daniellaikhter@gmail.com>
2016-08-16 19:26:15 -0400

    fix host waiting

--- checkers-project/src/main/java/com/cs451/checkers/HostWaitingThread.java
+++ checkers-project/src/main/java/com/cs451/checkers/HostWaitingThread.java
@@ -1,5 +1,6 @@
 package com.cs451.checkers;
 
+import javafx.application.Platform;
 import javafx.scene.web.WebEngine;
 
 public class HostWaitingThread extends Thread {
@@ -8,7 +9,12 @@ public class HostWaitingThread extends Thread {
 	
 	public void run() {
 		NormalNetworkManager.getInstance().host(port);
-		//Main.browser.webEngine.executeScript("host_continue()");
+		Platform.runLater(new Runnable() {
+		    @Override
+		    public void run() {
+		    	Main.browser.webEngine.executeScript("host_continue()");
+		    }
+		});
 	}
 
 	public HostWaitingThread(int port) {
